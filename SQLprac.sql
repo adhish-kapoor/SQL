111bb73f6832d461ba4c4bb60c714d0a6ea59f7b
Adhish Kapoor <adhish.kapoor@yahoo.co.in>
2018-02-18 17:35:28 +0530

    Update SQLprac.sql

--- SQLprac.sql
+++ SQLprac.sql
@@ -1,3 +1,6 @@
 --display name of students by sorting last 3 characters
 --if 3 characters are same then sort by their ID's
 select name from students where marks>75 order by right(name,3),id 
+
+--to replace all NULL values with 0
+select isnull(marks,0) from student
